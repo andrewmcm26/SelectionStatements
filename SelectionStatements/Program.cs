c52b670fb94c0309f739db2b4f3da790492f7068
Andrew McMahon <andrewmcm26@gmail.com>
2020-01-21 17:25:28 -0600

    If else and switch exercises, with while loop practice

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// IF-ELSE EXERCISE
+            var favNum = 1;
+           
+            Console.WriteLine("Try to guess my favorite number.");
+            var guess = int.Parse(Console.ReadLine());
+
+            do
+            {
+                if (guess < favNum)
+                {
+                    Console.WriteLine("Too low");
+                    Console.WriteLine("Would you like to guess again?");
+                    var guessAgain = Console.ReadLine();
+                    if (guessAgain == "yes")
+                    {
+                        Console.WriteLine("Okay take another shot");
+                        guess = int.Parse(Console.ReadLine());
+                        if (guess == 1)
+                        {
+                            Console.WriteLine("booyah!");
+                        }
+                    }
+                    else
+                        guess = 1;
+                }
+                else if (guess > favNum)
+                {
+                    Console.WriteLine("Too high");
+                    Console.WriteLine("Would you like to guess again?");
+                        var guessAgain = Console.ReadLine();
+                    if (guessAgain == "yes")
+                    {
+                        Console.WriteLine("Okay take another shot");
+                        guess = int.Parse(Console.ReadLine());
+                        if(guess == 1)
+                        {
+                            Console.WriteLine("booyah!");
+                        }
+                    }
+                    else
+                        guess = 1;
+                }
+
+                else /*if(guess == 1)*/
+                {
+                    Console.WriteLine("booyah!");
+                }
+            }
+            while (guess != favNum);
+
+
+
+        }
+
+
+            //SWITCH EXERCISES
+            //Console.WriteLine("Who goes there?");
+            //var name = Console.ReadLine();
+            //switch (name)
+            //{
+            //    case "michael":
+            //        Console.WriteLine("Sup big mike!");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine($"Sup {name}");
+
+            //        break;
+            //}
+
+            //Console.WriteLine("enter your favorite school subject");
+
+            //var subject = Console.ReadLine();
+            //switch(subject)
+            //{
+            //    case "math":
+            //        Console.WriteLine("that's my favorite subject too");
+            //        break;
+            //    default:
+            //        Console.WriteLine("that's not my favorite subject");
+            //        break;
+
+            //}
+        }
+    }
+
---
+++ SelectionStatements/Switch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Switch
+    {
+        static void Main(string[] args)
+        {
+
+            //SWITCH EXERCISES
+
+            var name = Console.ReadLine();
+            switch(name)
+            {
+                case "michael":
+                    Console.WriteLine("Sup big mike!");
+                    break;
+
+                default:
+                    Console.WriteLine($"Sup {name}");
+                    break;
+            }
+        }
+    }
+}
